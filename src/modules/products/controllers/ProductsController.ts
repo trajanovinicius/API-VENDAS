845597237f72c68f236154b3114e7d6a4cb974b5
trajanovinicius <viniluk9@gmail.com>
2023-01-05 00:02:37 -0300

    finalizado configuração das rotas para utilização

--- src/modules/products/controllers/ProductsController.ts
+++ src/modules/products/controllers/ProductsController.ts
@@ -5,7 +5,7 @@ import ListProductService from '../services/ListProductService';
 import ShowProductService from '../services/ShowProductService';
 import UpdateProductService from '../services/UpdateProductService';
 
-export default class ProductController {
+export default class ProductsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const listProducts = new ListProductService();
 
